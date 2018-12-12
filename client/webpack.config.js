ecd44d1267299ed1d63d6d98bb806aa18d28ae49
unknown <floriswarmenhoven@gmail.com>
2018-12-12 22:37:33 +0100

    Remove externals

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,12 +62,4 @@ module.exports = {
 		new HtmlWebpackPlugin({ template: resolve(__dirname, "src/index.html") }),
 		// inject <script> in html file.
 	],
-	// When importing a module whose path matches one of the following, just
-	// assume a corresponding global variable exists and use that instead.
-	// This is important because it allows us to avoid bundling all of our
-	// dependencies, which allows browsers to cache those libraries between builds.
-	// externals: {
-	// 	react: "React",
-	// 	"react-dom": "ReactDOM",
-	// },
 };
