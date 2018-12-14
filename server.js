3d81fd5ac025638e2df5ac503aec46d3e6ab2ddd
unknown <floriswarmenhoven@gmail.com>
2018-12-14 14:32:32 +0100

    Attempt build

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("/test", (req, res) => {
-	res.json({ hi: "hi" });
+	res.send(__dirname);
 });
+
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
