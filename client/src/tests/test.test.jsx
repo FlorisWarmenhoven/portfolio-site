cf294e63acf9ae84efbcf8da6b3dda8b6967732e
unknown <floriswarmenhoven@gmail.com>
2018-12-14 16:56:11 +0100

    Fix tests

--- client/src/tests/test.test.jsx
+++
@@ -1,4 +0,0 @@
-test("unit testing", () => {
-	expect(1).toBe(1);
-	expect(2).toBe(2);
-});
---
+++ client/src/tests/test.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { App } from '../components/App';
+// import Link from '../Link.react';
+// import renderer from 'react-test-renderer';
+
+test('unit testing', () => {
+  expect(1).toBe(1);
+  expect(<App />).toBeTruthy();
+  expect(2).toBe(2);
+});
