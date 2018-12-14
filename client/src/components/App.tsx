c6e1a5e085f30bb429c24904a7a556a27e3f7406
unknown <floriswarmenhoven@gmail.com>
2018-12-14 15:27:05 +0100

    Add automated deployment

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,7 +4,7 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hi, I'm the root</h1>
+        <h1>If the CircleCI pipeline is working - then you should see this.</h1>
       </div>
     );
   }
