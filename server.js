3700038967548427e908a01ace3b709eacf6b73a
unknown <floriswarmenhoven@gmail.com>
2018-12-14 14:40:56 +0100

    Attempt build fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/test", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+	res.sendFile(__dirname, "client/dist", "index.html");
 });
 
 const port = process.env.PORT || 8000;
