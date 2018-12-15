54e9c634584757dbc98510bf0affd7bd309e1d6c
unknown <floriswarmenhoven@gmail.com>
2018-12-15 10:59:57 +0100

    Setup backend to serve dist

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,17 @@ import { GraphQLServer } from "graphql-yoga";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
 import User from "./resolvers/User";
+import path from "path";
+import express from "express";
 import _ from "./env";
 import { prisma } from "./prisma";
+const port = process.env.PORT || 8000;
+const options = {
+	port,
+	endpoint: "/graphql",
+	playground: "/playground",
+	subscriptions: "/subscriptions",
+};
 
 const server = new GraphQLServer({
 	typeDefs: "./src/schema.graphql",
@@ -12,14 +21,23 @@ const server = new GraphQLServer({
 		Mutation,
 		User,
 	},
-	context: {},
+	// Add secret here
 });
 
+const publicPath = express.static(
+	path.join(__dirname, "..", "..", "client", "dist")
+);
+const indexPath = path.join(__dirname, "..", "..", "client", "dist/index.html");
+
+server.express.use(publicPath);
+server.express.use("*", (req, res) => {
+	res.sendFile(indexPath);
+});
+
+// TODO: remove - for testing purposes
 prisma.query
 	.users(null, "{id name email}")
 	.then(response => console.log(response))
 	.catch(error => console.log(error));
 
-server.start(() => {
-	console.log("The server is up!");
-});
+server.start(options);
