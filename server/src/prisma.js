8bf2eb12c04ff26fb3881bec8bd0d34477aa3938
unknown <floriswarmenhoven@gmail.com>
2018-12-15 11:41:41 +0100

    Add conditional path

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,7 +1,10 @@
 import { Prisma } from "prisma-binding";
+const pathToGraphql = process.env.PRODUCTION
+	? "server/src/generated/prisma.graphql"
+	: "src/generated/prisma.graphql";
 
 export const prisma = new Prisma({
-	typeDefs: "generated/prisma.graphql",
+	typeDefs: pathToGraphql,
 	endpoint: process.env.PRISMA_ENDPOINT,
 	secret: process.env.PRISMA_SECRET,
 });
