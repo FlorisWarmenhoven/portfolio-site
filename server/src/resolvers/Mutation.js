4ae7601ee06fa32c37294f18b8f172148f48f280
unknown <floriswarmenhoven@gmail.com>
2018-12-15 16:20:28 +0100

    Setup authentication with JWT

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,24 +1,33 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const Mutation = {
 	async createUser(parent, args, { prisma }, info) {
-		const { password } = args.data;
+		const { password, email } = args.data;
+		const lowercaseEmail = args.data.email.toLowerCase();
 
 		if (password.length < 8) {
 			throw new Error("Password must be at least 8 characters long.");
 		}
 
+		const emailInUse = await prisma.exists.User({ email: lowercaseEmail });
+		if (emailInUse) {
+			throw new Error("Email is already in use.");
+		}
+
 		const hashedPassword = await bcrypt.hash(password, 10);
-		console.log("args:", args.data);
-		return prisma.mutation.createUser(
-			{
-				data: {
-					...args.data,
-					password: hashedPassword,
-				},
+		const user = await prisma.mutation.createUser({
+			data: {
+				...args.data,
+				email: lowercaseEmail,
+				password: hashedPassword,
 			},
-			info
-		);
+		});
+
+		return {
+			user,
+			token: jwt.sign({ userId: user.id }, process.env.JWT_SECRET),
+		};
 	},
 	deleteUser(parent, args, { prisma }, info) {
 		return prisma.mutation.deleteUser({ where: { id: args.id } });
@@ -26,6 +35,31 @@ const Mutation = {
 	async createProject(parent, args, { prisma }, info) {
 		const response = await prisma.mutation.createProject(args);
 		return response;
+	},
+	async login(parent, args, { prisma }, info) {
+		const user = await prisma.query.user({
+			where: {
+				email: args.data.email.toLowerCase(),
+			},
+		});
+
+		if (!user) {
+			throw new Error("Unable to login. Check your credentials.");
+		}
+
+		const passwordMatches = await bcrypt.compare(
+			args.data.password,
+			user.password
+		);
+
+		if (!passwordMatches) {
+			throw new Error("Unable to login. Check your credentials.");
+		}
+
+		return {
+			user,
+			token: jwt.sign({ userId: user.id }, process.env.JWT_SECRET),
+		};
 	},
 };
 
