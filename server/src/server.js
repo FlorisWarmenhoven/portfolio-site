cc3f9ae6475bd89733f2b7111055d1dcbf5ccb2a
unknown <floriswarmenhoven@gmail.com>
2018-12-15 12:23:14 +0100

    Exclude api calls from being served static html

--- server/src/server.js
+++ server/src/server.js
@@ -7,18 +7,24 @@ import express from "express";
 import _ from "./env";
 import { prisma } from "./prisma";
 
+// Variable port for Heroku or local development
 const port = process.env.PORT || 8000;
-const options = {
-	port,
-	endpoint: "/graphql",
-	playground: "/playground",
-	subscriptions: "/subscriptions",
-};
+
+// Variable schemaPath for Heroku or local development
 const schemaPath =
 	process.env.PRODUCTION === "true"
 		? "server/src/schema.graphql"
 		: "src/schema.graphql";
 
+// GraphQL Yoga server options
+const options = {
+	port,
+	endpoint: "/api/graphql",
+	playground: "/api/playground",
+	subscriptions: "/api/subscriptions",
+};
+
+// GraphQL Yoga server
 const server = new GraphQLServer({
 	typeDefs: schemaPath,
 	resolvers: {
@@ -26,18 +32,33 @@ const server = new GraphQLServer({
 		Mutation,
 		User,
 	},
-
-	// Add secret here
 });
 
+// File path to client dist folder
 const publicPath = express.static(
 	path.join(__dirname, "..", "..", "client", "dist")
 );
+// File path to index.html in client dist folder
 const indexPath = path.join(__dirname, "..", "..", "client", "dist/index.html");
 
+// Serve static files from client dist folder
 server.express.use(publicPath);
-server.express.use("*", (req, res) => {
-	res.sendFile(indexPath);
+
+// if the request is for the api - pass it through
+// otherwise send the index.html
+server.express.use((req, res, next) => {
+	const { originalUrl } = req;
+
+	// TODO: Use RegEx to allow /api/*
+	if (
+		originalUrl === "/api/graphql" ||
+		originalUrl === "/api/playground" ||
+		originalUrl === "/api/subscriptions"
+	) {
+		return next();
+	} else {
+		res.sendFile(indexPath);
+	}
 });
 
 // TODO: remove - for testing purposes
