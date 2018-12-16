992453bad7e9a6b40b51f5fa057cc39551c6952c
unknown <floriswarmenhoven@gmail.com>
2018-12-16 17:42:34 +0100

    Restructure env file loading and root npm start

--- server/start.js
+++
@@ -1,9 +0,0 @@
-// Injects .env files into process.env
-require("dotenv").config({ silent: true });
-
-require("babel-register")({
-	presets: ["env", "es2015", "stage-0"],
-});
-
-// Import the rest of our application.
-module.exports = require("./src/index.js");
