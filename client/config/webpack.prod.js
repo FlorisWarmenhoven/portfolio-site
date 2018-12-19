0f349ae44d8fb6ae132a0e8addcc1cd56e73f218
unknown <floriswarmenhoven@gmail.com>
2018-12-19 18:27:21 +0100

    Add css loading to production webpack

--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -23,6 +23,12 @@ module.exports = {
 
 			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
 			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+			// Load in CSS
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
 		],
 	},
 	plugins: [
