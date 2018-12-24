696b70203103cf3ee18c1025b03109db04d39d10
Floris Warmenhoven <FlorisWarmenhoven@gmail.com>
2018-12-24 11:02:14 +0100

    Add mobile navbar

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,14 +4,16 @@ import Landing from "./Landing";
 import styled from "../../types/styled-components";
 import About from "./About";
 
-const StyledApp = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
+const StyledApp = styled.div``;
 
 const StyledPages = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 215px;
+
+  @media (max-width: 800px) {
+    margin-left: 0px;
+  }
 `;
 export class App extends Component {
 	render() {
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,123 +1,14 @@
 import React, { Component } from "react";
-import styled from "../../types/styled-components";
-import {
-	FaHome,
-	FaUser,
-	FaBriefcase,
-	FaLightbulb,
-	FaChartBar,
-	FaPhone,
-	FaFileAlt,
-} from "react-icons/fa";
-
-const StyledNavigationBar = styled.div`
-  @media (max-width: 800px) {
-    display: none;
-    width: 0%;
-  }
-
-  font-family: "PT Sans", sans-serif;
-  background-color: #232a34;
-  min-width: 215px;
-  min-height: 100vh;
-  color: #f4ad24;
-
-  a {
-    text-decoration: none;
-    color: inherit;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    width: 100%;
-  }
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  li {
-    list-style: none;
-    height: 108.5px;
-    width: 100%;
-    border-top-width: 0px;
-    border-right-width: 0px;
-    border-left-width: 0px;
-    border-bottom-color: #303947;
-    border-bottom-width: 1px;
-    border-style: solid;
-
-    .icon {
-      font-size: 39px;
-      margin-bottom: 5px;
-    }
-  }
-
-  #logo {
-    height: 100%;
-  }
-`;
-
-const StyledLogoArea = styled.div`
-  height: 172px;
-  width: 100%;
-`;
+import DefaultNavbar from "./Navbar/DefaultNavbar";
+import MobileNavbar from "./Navbar/MobileNavbar";
 
 export default class Navbar extends Component {
 	render() {
 		return (
-			<StyledNavigationBar>
-				<StyledLogoArea>
-					<li id="logo">
-						<a href="">LOGO</a>
-					</li>
-				</StyledLogoArea>
-				<ul>
-					<li>
-						<a href="#">
-							<FaHome className="icon" />
-							Home
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaUser className="icon" />
-							About
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaBriefcase className="icon" />
-							Work
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaLightbulb className="icon" />
-							Experience
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaChartBar className="icon" />
-							Skill
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaFileAlt className="icon" />
-							Cv
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<FaPhone className="icon" />
-							Contact
-						</a>
-					</li>
-				</ul>
-			</StyledNavigationBar>
+			<>
+				<DefaultNavbar />
+				<MobileNavbar />
+			</>
 		);
 	}
 }
