aa89eb2b1e83a9fa089d5953013ac748395a879d
Floris Warmenhoven <floriswarmenhoven@gmail.com>
2019-02-02 17:57:06 +0100

    Fix tsconfig giving Jest conflictions

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	roots: ["<rootDir>/tests"],
 	transform: {
-		".*.tsx?$": "ts-jest"
+		".*.tsx?$": "ts-jest",
 	},
 	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 	moduleNameMapper: {
 		".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-			"identity-obj-proxy"
-	}
+			"identity-obj-proxy",
+	},
 };
