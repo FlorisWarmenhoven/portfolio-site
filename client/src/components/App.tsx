4edd17e3b2ac28ebea357a73ad188e6af2bdd69b
Floris Warmenhoven <floriswarmenhoven@gmail.com>
2019-02-02 17:23:17 +0100

    Fix import for unit test

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Routes from "../routes";
+import Routes from "../Routes";
 import { BrowserRouter } from "react-router-dom";
 
 export class App extends Component {
