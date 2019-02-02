9824517f20f2453dd8242eaa1e82dfcdf7b96122
Floris Warmenhoven <floriswarmenhoven@gmail.com>
2019-02-02 19:03:42 +0100

    Adjust test for test coverage

--- client/tests/test.test.tsx
+++ client/tests/test.test.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import Navbar from "../src/components/portfolio/Navbar";
 
 test("unit testing", () => {
 	expect(1).toBe(1);
+	expect(<Navbar />).toBeTruthy();
 	expect(2).toBe(2);
 });
 ``;
