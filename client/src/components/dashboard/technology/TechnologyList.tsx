d22d9b0a4f1d5c706f60e4a0cfb0c2f703cad1a7
Floris Warmenhoven <floriswarmenhoven@gmail.com>
2019-02-08 21:07:26 +0100

    Refactor to FC

--- client/src/components/dashboard/technology/TechnologyList.tsx
+++ client/src/components/dashboard/technology/TechnologyList.tsx
@@ -1,37 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, FC } from "react";
 import { Query } from "react-apollo";
-import gql from "graphql-tag";
 import { TechnologyItem } from "./TechnologyItem";
+import { GET_TECHNOLOGIES } from "../../../graphql/queries";
 
-const GET_TECHNOLOGIES = gql`
-	{
-		technologies {
-			id
-			name
-			iconUrl
-		}
-	}
-`;
+interface Props {}
 
-export default class TechnologyList extends Component {
-	render() {
-		return (
-			<Query query={GET_TECHNOLOGIES}>
-				{({ loading, error, data }) => {
-					if (loading) return "Loading...";
-					if (error) return `Error! ${error.message}`;
+export const TechnologyList: FC<Props> = props => {
+	return (
+		<Query query={GET_TECHNOLOGIES}>
+			{({ loading, error, data }) => {
+				if (loading) return "Loading...";
+				if (error) return `Error! ${error.message}`;
 
-					return (
-						<div>
-							{data.technologies.map((technology: any) => (
-								<div key={technology.id}>
-									<TechnologyItem technology={technology} />
-								</div>
-							))}
-						</div>
-					);
-				}}
-			</Query>
-		);
-	}
-}
+				return (
+					<div>
+						{data.technologies.map((technology: any) => (
+							<div key={technology.id}>
+								<TechnologyItem technology={technology} />
+							</div>
+						))}
+					</div>
+				);
+			}}
+		</Query>
+	);
+};
