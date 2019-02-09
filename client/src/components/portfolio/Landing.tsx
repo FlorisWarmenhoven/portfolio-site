71be3c89da0a93a4f5aa0820262913557481e61c
Floris Warmenhoven <floriswarmenhoven@gmail.com>
2019-02-09 14:28:56 +0100

    Add under development notice

--- client/src/components/portfolio/Landing.tsx
+++ client/src/components/portfolio/Landing.tsx
@@ -5,7 +5,14 @@ const landingImage = require("../../assets/landing-image.jpg");
 interface Props {}
 
 export const Landing: FC<Props> = props => {
-	return <StyledLanding />;
+	return (
+		<StyledLanding>
+			<h1>
+				This site is still under development. All images and text are
+				placeholder
+			</h1>
+		</StyledLanding>
+	);
 };
 
 const StyledLanding = styled.div`
@@ -16,6 +23,14 @@ const StyledLanding = styled.div`
 	background-attachment: fixed;
 	background-position: center;
 	background-image: url(${landingImage});
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	h1 {
+		color: yellow;
+	}
 
 	@media (max-width: 800px) {
 		max-height: 1100px;
