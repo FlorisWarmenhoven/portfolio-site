dbb5c1f7d60478538f2459d8b5f99bd1940269ab
Floris <floriswarmenhoven@gmail.com>
2019-02-26 15:36:10 +0100

    Package update

--- client/tests/test.test.tsx
+++ client/tests/test.test.tsx
@@ -2,8 +2,7 @@ import * as React from "react";
 import { Navbar } from "../src/components/portfolio/Navbar";
 
 test("unit testing", () => {
-	expect(1).toBe(1);
-	expect(<Navbar />).toBeTruthy();
-	expect(2).toBe(2);
+  expect(1).toBe(1);
+  expect(<Navbar />).toBeTruthy();
+  expect(2).toBe(2);
 });
-``;
